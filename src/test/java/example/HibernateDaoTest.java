3402b8d11fe4391e198343b2d64ed84f0d218b96
Lyle Hanson <lyle2.0@gmail.com>
2011-01-28 22:57:05 -0600

    Got it working with @Autowire and @ContextConfiguration

--- src/test/java/example/HibernateDaoTest.java
+++ src/test/java/example/HibernateDaoTest.java
@@ -1,33 +1,25 @@
 package example;
 
-import example.dao.EntityDao;
 import static org.junit.Assert.*;
 import org.junit.Test;
-
-import example.om.EntityA;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//TODO: this doesn't ever seem to work!
+import example.dao.EntityDao;
+import example.om.EntityA;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:testApplicationContext.xml"})
-public class HibernateDaoTest extends AbstractDependencyInjectionSpringContextTests {
+public class HibernateDaoTest {
     @Autowired
     private EntityDao dao;
-
-//    @Override
-//    protected String[] getConfigLocations() {
-//        return new String[] { "testApplicationContext.xml" };
-//    }
 
     @Test
     public void testStoreAndRetrieveEntity() {
         EntityA entity = new EntityA();
         assertNotNull(this.dao);
-        System.out.println("********DAO: " + this.dao);
         assertNotNull(entity);
     }
 
