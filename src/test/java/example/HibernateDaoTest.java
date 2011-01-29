382fc902e3c53a24c167c42cc45b3a001ac2e952
Lyle Hanson <lyle2.0@gmail.com>
2011-01-28 22:47:03 -0600

    This works from command line but not from Netbeans

--- src/test/java/example/HibernateDaoTest.java
+++ src/test/java/example/HibernateDaoTest.java
@@ -5,18 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import example.om.EntityA;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ContextConfiguration(locations={"testApplicationContext.xml"})
-public class HibernateDaoTest /*extends AbstractDependencyInjectionSpringContextTests */{
+//TODO: this doesn't ever seem to work!
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:testApplicationContext.xml"})
+public class HibernateDaoTest extends AbstractDependencyInjectionSpringContextTests {
     @Autowired
     private EntityDao dao;
+
+//    @Override
+//    protected String[] getConfigLocations() {
+//        return new String[] { "testApplicationContext.xml" };
+//    }
 
     @Test
     public void testStoreAndRetrieveEntity() {
         EntityA entity = new EntityA();
         assertNotNull(this.dao);
+        System.out.println("********DAO: " + this.dao);
         assertNotNull(entity);
     }
 
